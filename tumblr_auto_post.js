8545e8fc65b04afc2d70b82d952b32ba84d06cf4
Roman Hnatiuk <roman.hnatiuk02@gmail.com>
2023-12-07 01:19:36 +0200

    fix

--- tumblr_auto_post.js
+++ tumblr_auto_post.js
@@ -1,114 +1,114 @@
-import OAuth from 'oauth';
+import OAuth from "oauth";
 
 const consumerKey = 'qSjWrsq1wLRd5fmwxdkYwWO9PFXBxgYLfo3uyv8EMll6nYwOPN';
 const consumerSecret = 'XAZ4oOs8q5zhjKY4IJSkc8GSDQu2cRE7pSiQwVtZ4Dukv03nLF';
 const accessToken = 'G8WVRF9jySZEBo3JaZNPD8eteXScj24aVKJK4kfWcuMuA5eOz0';
 const accessTokenSecret = '5ZhZ4oqaLYYjD0RNWRDdOLVUBgKvoypTOvMmhyYwDhNv94PnPs';
 
-const tumblrBlogIdentifier = 'sportscoreio.tumblr.com';
-
 const postedMatches = new Set();
 
 function fetchData() {
-    fetch('https://sportscore.io/api/v1/football/matches/?match_status=live&sort_by_time=false&page=0', {
-        method: 'GET',
-        headers: {
-            "accept": "application/json",
-            'X-API-Key': 'uqzmebqojezbivd2dmpakmj93j7gjm',
-        },
+  fetch(
+    "https://sportscore.io/api/v1/football/matches/?match_status=live&sort_by_time=false&page=0",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "X-API-Key": "uqzmebqojezbivd2dmpakmj93j7gjm",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      processData(data.match_groups);
     })
-    .then(response => response.json())
-    .then(data => {
-        processData(data.match_groups);
-    })
-    .catch(error => {
-        console.error('Error:', error);
+    .catch((error) => {
+      console.error("Error:", error);
     });
 }
 
 function processData(matchGroups) {
-    try {
-        if (!Array.isArray(matchGroups)) {
-            console.error('Invalid matchGroups:', matchGroups);
-            return;
-        }
-
-        matchGroups.forEach(matchGroup => {
-            getMatch(matchGroup);
-        });
-    } catch (error) {
-        console.error('Error processing data:', error);
+  try {
+    if (!Array.isArray(matchGroups)) {
+      console.error("Invalid matchGroups:", matchGroups);
+      return;
     }
+
+    matchGroups.forEach((matchGroup) => {
+      getMatch(matchGroup);
+    });
+  } catch (error) {
+    console.error("Error processing data:", error);
+  }
 }
 
 async function getMatch(matchGroup) {
-    try {
-        const competition = matchGroup['competition']['name'];
+  try {
+    const competition = matchGroup.competition.name;
 
-        matchGroup['matches'].forEach(match => {
-            const matchId = match.id; // Use a unique identifier for each match
+    matchGroup.matches.forEach((match) => {
+      const matchId = match.id;
 
-            // Check if the match has already been posted
-            if (!postedMatches.has(matchId)) {
-                const homeTeam = match['home_team']['name'];
-                const awayTeam = match['away_team']['name'];
-                const league = competition;
-                const matchLink = match['url'];
-                const venueName = match['venue']['name'] || '';
+      if (!postedMatches.has(matchId)) {
+        const homeTeam = match.home_team.name;
+        const awayTeam = match.away_team.name;
+        const league = competition;
+        const matchLink = match.url;
+        const venueName = match.venue ? match.venue.name : "";
 
-                let postContent = `🎌 Match Started! 🎌\n\n`;
-                postContent += `💥⚽️💥 ${homeTeam} vs ${awayTeam} League: ${league} 💥⚽️💥\n\n`;
-                postContent += `Watch Now on SportScore: ${matchLink}\n\n`;
-                postContent += `Venue: ${venueName}\n\n`;
+        let postContent = `🎌 Match Started! 🎌\n\n`;
+        postContent += `💥⚽️💥 ${homeTeam} vs ${awayTeam} League: ${league} 💥⚽️💥\n\n`;
+        postContent += `Watch Now on SportScore: ${matchLink}\n\n`;
+        postContent += `Venue: ${venueName}\n\n`;
 
-                setTimeout(() => {
-                    postToTumblr(postContent);
-                  }, 60000);
+        // Introduce a delay of 1 minute before posting
+        setTimeout(() => {
+          postToTumblr(postContent);
+        }, 60000);
 
-                // Add the match ID to the set of posted matches
-                postedMatches.add(matchId);
-            }
-        });
-    } catch (error) {
-        console.error('Error getting match:', error.message);
-    }
+        postedMatches.add(matchId);
+      }
+    });
+  } catch (error) {
+    console.error("Error getting match:", error.message);
+  }
 }
 
 async function postToTumblr(postText) {
-    try {
-        const oauth = new OAuth.OAuth(
-            null,
-            null,
-            consumerKey,
-            consumerSecret,
-            '1.0A',
-            null,
-            'HMAC-SHA1'
-        );
+  try {
+    const oauth = new OAuth.OAuth(
+      null,
+      null,
+      consumerKey,
+      consumerSecret,
+      "1.0A",
+      null,
+      "HMAC-SHA1"
+    );
 
-        const postParams = {
-            type: 'text',
-            title: 'Match started!',
-            body: postText,
-        };
+    const postParams = {
+      type: "text",
+      title: "Match started!",
+      body: postText,
+    };
 
-        oauth.post(
-            `https://api.tumblr.com/v2/blog/${tumblrBlogIdentifier}/post`,
-            accessToken,
-            accessTokenSecret,
-            postParams,
-            '',
-            (error, data) => {
-                if (error) {
-                    console.error('Error posting to Tumblr:', error);
-                } else {
-                    console.log('Post successful:', data);
-                }
-            }
-        );
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    oauth.post(
+      `https://api.tumblr.com/v2/blog/${tumblrBlogIdentifier}/post`,
+      accessToken,
+      accessTokenSecret,
+      postParams,
+      "",
+      (error, data) => {
+        if (error) {
+          console.error("Error posting to Tumblr:", error);
+        } else {
+          console.log("Post successful:", data);
+        }
+      }
+    );
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 setInterval(fetchData, 60000);
